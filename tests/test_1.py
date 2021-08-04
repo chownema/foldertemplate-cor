e1d4415574d4cf2873ebbc194f703512849974e9
WhatIsOO <chownema@gmail.com>
2021-08-04 21:54:26 +1200

    Added factory tests

--- tests/test_1.py
+++ tests/test_1.py
@@ -6,9 +6,43 @@ sys.path.append(
 from instructions_service import InstructionsService
 import unittest
 
-class TestFolder(unittest.TestCase):
+# Testing the logical objects in the system
+class TestInterfaceObjects(unittest.TestCase):
+    def setUp(self):
+        # Objects
+        # request
+        # response
+        pass
+    
+    def test_check_folder_object_properties(self):
+        folder = TemplateFolderFactory.get_folder()
+        self.assertEqual("name" in folder, True, "folder contains name")
+        self.assertEqual(isinstance(folder["name"], str)  , True, "folder contains name")
+        self.assertEqual("children" in folder, True, "folder contains folder list")
+        self.assertEqual(isinstance(folder["children"], list)  , True, "folder contains name")
+        self.assertEqual("exists" in folder, True, "folder contains boolean for existing")
+        self.assertEqual(isinstance(folder["exists"], bool)  , True, "folder contains boolean existing")
+        pass
+
+    def test_check_request_object_properties(self):
+        request = TemplateInterfaceFactory.get_request()
+        self.assertEqual("name" in request, True, "request contains name")
+        self.assertEqual(isinstance(request["name"], str)  , True, "request contains name")
+        self.assertEqual(isinstance(request["success"], bool)  , True, "request contains boolean success")
+        pass
+
+    def test_check_response_object_properties(self):
+        response = TemplateInterfaceFactory.get_response()
+        self.assertEqual("name" in response, True, "response contains name")
+        self.assertEqual(isinstance(response["name"], str)  , True, "response contains name") 
+        self.assertEqual(isinstance(response["success"], bool)  , True, "request contains boolean success")
+        pass
+
+
+class TestFolderObject(unittest.TestCase):
     def setUp(self):
         # load test data
+        # folder
         pass
 
     def test_check_exclude_folders(self):
@@ -32,10 +66,6 @@ class TestFolder(unittest.TestCase):
                          "Instruction should have a folder Structure part in of dict")
         pass
 
-    def test_check_for_folder_child_struct_config(self):
-        # Check for folder child structure provided in the instruction dict if provided 
-        pass
-
     def test_check_for_folder_struct_config_missing_folder_or_file(self):
         # Check for a mising folder or file according to the folder structure at an instruction point
         pass
@@ -50,18 +80,6 @@ class TestFolder(unittest.TestCase):
 
     def test_loop_through_all_folders_parent_and_children_folders(self):
         # check response object has checked off the items in the list of children & parent folders given
-        pass
-
-
-class TestCreateLogic(unittest.TestCase):
-    def setUp(self):
-        # load test data
-        pass
-
-    def test_create_missing_folder(self):
-        pass
-
-    def test_create_missing_(self):
         pass
 
 if __name__ == '__main__':
